2e67593bcc29512ee9530c47426d76717db4df40
Ali Madooei <madooei@jhu.edu>
2020-02-25 12:47:55 -0500

    send data to courses.hbs

--- src/main/java/WebServer.java
+++ src/main/java/WebServer.java
@@ -1,17 +1,30 @@
 
+import dao.CourseDao;
+import dao.InMemoryCourseDao;
+import model.Course;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static spark.Spark.*;
 
 public class WebServer {
   public static void main(String[] args) {
+
+    CourseDao courseDao = new InMemoryCourseDao();
+    courseDao.add(new Course("OOSE", "jhu-oose.com"));
+    courseDao.add(new Course("Gateway", "jhu-gateway.com"));
+
     get("/", ((request, response) -> {
       return new ModelAndView(null, "index.hbs");
     }), new HandlebarsTemplateEngine());
 
     get("/courses", ((request, response) -> {
-      return new ModelAndView(null, "courses.hbs");
+      Map<String, Object> model = new HashMap<>();
+      model.put("courseList", courseDao.findAll());
+      return new ModelAndView(model, "courses.hbs");
     }),  new HandlebarsTemplateEngine());
   }
 }
