cfe4327f9386b2b64ae5d5d3b789d665c8652767
Ali Madooei <madooei@jhu.edu>
2020-02-25 12:27:27 -0500

    Add index.hbs

--- src/main/java/WebServer.java
+++ src/main/java/WebServer.java
@@ -1,10 +1,14 @@
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 import static spark.Spark.*;
 
 public class WebServer {
   public static void main(String[] args) {
     get("/", ((request, response) -> {
-      return "Welcome to CourseReVU App.";
-    }));
+      return new ModelAndView(null, "index.hbs");
+    }), new HandlebarsTemplateEngine());
   }
 }
+
